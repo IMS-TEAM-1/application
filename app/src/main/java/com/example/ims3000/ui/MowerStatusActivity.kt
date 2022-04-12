e05458c6ece736b6f427c608592c4105420e569f
Ivin benjamin <55826241+IvinBenjamin@users.noreply.github.com>
2022-04-12 16:01:23 +0200

    battery bar removed

--- app/src/main/java/com/example/ims3000/ui/MowerStatusActivity.kt
+++ app/src/main/java/com/example/ims3000/ui/MowerStatusActivity.kt
@@ -8,18 +8,11 @@ import com.example.ims3000.R
 import kotlin.concurrent.fixedRateTimer
 
 class MowerStatusActivity : AppCompatActivity() {
-    private var batteryLevel = 0
-    private val progress_bar: ProgressBar = findViewById(R.id.progress_bar)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mower_status)
-        fixedRateTimer("updateBatteryLevel", false, 0L, 60 * 1000){
-            updateProgressBar()
-        }
-    }
 
-    private fun updateProgressBar() {
-        progress_bar.progress = batteryLevel
-    }
+        }
 }
+
