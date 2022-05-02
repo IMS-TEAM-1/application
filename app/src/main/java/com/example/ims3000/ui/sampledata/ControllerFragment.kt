5162da6e5ea536b8ccc494b245d3c2d532934e8e
Falco1997 <filipgolf1997@gmail.com>
2022-05-02 10:28:03 +0200

    ControllerFragment Merged
    Todo: start on visualize
    fix most http requests

--- app/src/main/java/com/example/ims3000/ui/sampledata/ControllerFragment.kt
+++ app/src/main/java/com/example/ims3000/ui/sampledata/ControllerFragment.kt
@@ -16,10 +16,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [RemoteControllerFragment.newInstance] factory method to
+ * Use the [ControllerFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class RemoteControllerFragment : Fragment() {
+class ControllerFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -37,7 +37,7 @@ class RemoteControllerFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_remote_controller, container, false)
+        return inflater.inflate(R.layout.fragment_controller, container, false)
 
     }
 
@@ -67,7 +67,7 @@ class RemoteControllerFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            RemoteControllerFragment().apply {
+            ControllerFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
