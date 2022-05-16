23217ed18db6a9acfe62fd870df3925245f8fe65
Ivin benjamin <55826241+IvinBenjamin@users.noreply.github.com>
2022-05-16 11:12:04 +0200

    Bluetooth according to the MVVM Architecture, fixed sending data via Bluetooth

--- app/src/main/java/com/example/ims3000/ui/fragments/ControllerFragment.kt
+++ app/src/main/java/com/example/ims3000/ui/fragments/ControllerFragment.kt
@@ -2,33 +2,29 @@ package com.example.ims3000.ui.fragments
 
 import android.app.Activity.RESULT_CANCELED
 import android.app.Activity.RESULT_OK
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothDevice
-import android.bluetooth.BluetoothManager
 import android.bluetooth.BluetoothSocket
-import android.bluetooth.le.BluetoothLeScanner
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
+import android.view.View.OnTouchListener
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.ims3000.R
 import com.example.ims3000.databinding.FragmentControllerBinding
+import com.example.ims3000.ui.viewmodels.ControllerViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import java.io.IOException
-import java.io.InputStream
-import java.io.OutputStream
-import java.util.*
 
 
 @AndroidEntryPoint
 open class ControllerFragment : Fragment(R.layout.fragment_controller) {
 
-    //lateinit var viewModel: HomeViewModel
     private var _binding: FragmentControllerBinding? = null
-    private val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
-    private val myUUID = UUID.fromString("94f39d29-7d6d-437d-973b-fba39e49d4ee")
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -36,30 +32,84 @@ open class ControllerFragment : Fragment(R.layout.fragment_controller) {
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        if (!btAdapter?.isEnabled!!) {
-            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
-        }
-
-        val discoverableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE)
-        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION,
-            DISCOVERABLE_DURATION)
-        startActivityForResult(discoverableIntent, REQUEST_DISCOVERABLE_BT)
-
         _binding = FragmentControllerBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val forwardButton = view?.findViewById<Button>(R.id.forward_button)
+        val backwardButton = view?.findViewById<Button>(R.id.backward_button)
+        val turnRightButton = view?.findViewById<Button>(R.id.turn_right_button)
+        val turnLeftButton = view?.findViewById<Button>(R.id.turn_left_button)
+
+        val controllerViewModel = ViewModelProvider(this)[ControllerViewModel::class.java]
+        controllerViewModel.onActivityResult(0, 0)
+        controllerViewModel.InitializeSocket()
+
+        //forwardButton?.setOnClickListener {
+        //    Toast.makeText(context, "Forward", Toast.LENGTH_SHORT).show()
+        //    controllerViewModel.write(1)
+        //}
+
+       forwardButton?.setOnTouchListener(OnTouchListener { v, event ->
+           if (event.action == MotionEvent.ACTION_DOWN){
+               controllerViewModel.write(1)
+               Log.d("Pressed", "Button pressed")
+           }
+           else if (event.action == MotionEvent.ACTION_UP) {
+           Log.d("Released", "Button released")
+           controllerViewModel.write(0)
+            }
+           false
+       })
+
+        backwardButton?.setOnTouchListener(OnTouchListener { v, event ->
+            if (event.action == MotionEvent.ACTION_DOWN){
+                controllerViewModel.write(2)
+                Log.d("Pressed", "Button pressed")
+            }
+            else if (event.action == MotionEvent.ACTION_UP) {
+                Log.d("Released", "Button released")
+                controllerViewModel.write(0)
+            }
+            false
+        })
+
+        turnRightButton?.setOnTouchListener(OnTouchListener { v, event ->
+            if (event.action == MotionEvent.ACTION_DOWN){
+                controllerViewModel.write(3)
+                Log.d("Pressed", "Button pressed")
+            }
+            else if (event.action == MotionEvent.ACTION_UP) {
+                Log.d("Released", "Button released")
+                controllerViewModel.write(0)
+            }
+            false
+        })
+
+        turnLeftButton?.setOnTouchListener(OnTouchListener { v, event ->
+            if (event.action == MotionEvent.ACTION_DOWN){
+                controllerViewModel.write(4)
+                Log.d("Pressed", "Button pressed")
+            }
+            else if (event.action == MotionEvent.ACTION_UP) {
+                Log.d("Released", "Button released")
+                controllerViewModel.write(0)
+            }
+            false
+        })
+
 
     }
+
+
+
     companion object {
-        private const val REQUEST_ENABLE_BT = 1
-        private const val REQUEST_DISCOVERABLE_BT = 2
-        private const val DISCOVERABLE_DURATION = 120
-        private const val PERMISSION_REQUEST_COARSE_LOCATION = 1
-        private var btManager: BluetoothManager? = null
-        private var btAdapter: BluetoothAdapter? = null
-        private var btSocket : BluetoothSocket? = null
-        private var inputStream : InputStream? = null
-        private var outputStream : OutputStream? = null
+        const val REQUEST_ENABLE_BT = 1
+        internal const val PERMISSION_REQUEST_COARSE_LOCATION = 1
+        var btSocket : BluetoothSocket? = null
     }
 
 
@@ -74,42 +124,7 @@ open class ControllerFragment : Fragment(R.layout.fragment_controller) {
             }
         }
     }
-    private fun InitializeSocket(btDevice : BluetoothDevice){
-        try {
-            btSocket = btDevice.createRfcommSocketToServiceRecord(myUUID);
-        } catch (IOException e) {
-            //Error
-        }
 
-        try {
-            btSocket.connect()
-        } catch (connEx: IOException) {
-            try {
-                btSocket.close()
-            } catch (closeException: IOException) {
-                //Error
-            }
-        }
-
-        if (btSocket != null && btSocket!!.isConnected) {
-            //Socket is connected, now we can obtain our IO streams
-        }
-
-        try {
-            inputStream = btSocket?.inputStream
-            outputStream = btSocket?.outputStream
-        } catch (e: IOException) {
-            //Error
-        }
-
-    }
-    open fun write(bytes: ByteArray?) {
-        try {
-            outputStream?.write(bytes)
-        } catch (e: IOException) {
-            //Error
-        }
-    }
 
 
 }
--- app/src/main/java/com/example/ims3000/ui/viewmodels/ControllerViewModel.kt
+++ app/src/main/java/com/example/ims3000/ui/viewmodels/ControllerViewModel.kt
@@ -1,13 +1,111 @@
 package com.example.ims3000.ui.viewmodels
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.Application
+import android.bluetooth.BluetoothDevice
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import com.example.ims3000.api.ApiRepository
+import com.example.ims3000.ui.fragments.ControllerFragment
+import com.example.ims3000.ui.fragments.ControllerFragment.Companion.btSocket
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+import java.util.*
 import javax.inject.Inject
 
-@HiltViewModel
-class ControllerViewModel @Inject constructor(private val apiRepository: ApiRepository): ViewModel() {
+
+class ControllerViewModel(application: Application) : AndroidViewModel(application) {
 
     // should contain interactions
 
+    private val myUUID = UUID.fromString("94f39d29-7d6d-437d-973b-fba39e49d4ee")
+    private var inputStream : InputStream? = null
+    private var outputStream : OutputStream? = null
+    private val btDevice : BluetoothDevice? = null
+
+
+    fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            ControllerFragment.PERMISSION_REQUEST_COARSE_LOCATION -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    println("coarse location permission granted")
+                } else {
+                    val builder = AlertDialog.Builder(getApplication())
+                    builder.setTitle("Functionality limited")
+                    builder.setMessage("Since location access has not been granted, this app will not be able to discover BLE beacons")
+                    builder.setPositiveButton(android.R.string.ok, null)
+                    builder.setOnDismissListener { }
+                    builder.show()
+                }
+                return
+            }
+        }
+    }
+
+    @Override
+    fun onActivityResult(requestCode: Int, resultCode: Int) {
+        if (requestCode == ControllerFragment.REQUEST_ENABLE_BT) {
+            if (resultCode == Activity.RESULT_OK) {
+                // Bluetooth was enabled
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                // Bluetooth was not enabled
+            }
+        }
+    }
+
+    open fun InitializeSocket(){
+        try {
+            if (ActivityCompat.checkSelfPermission(getApplication(),
+                    Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED
+            )
+                btSocket = btDevice?.createRfcommSocketToServiceRecord(myUUID)
+
+        } catch (e: IOException) {
+            //Error
+            Log.e("Socket Error", e.toString())
+        }
+
+        try {
+            btSocket?.connect()
+        } catch (connEx: IOException) {
+            try {
+                btSocket?.close()
+            } catch (closeException: IOException) {
+                //Error
+            }
+        }
+
+        if (btSocket != null && btSocket!!.isConnected) {
+            //Socket is connected, now we can obtain our IO streams
+        }
+
+        try {
+            inputStream = btSocket?.inputStream
+            outputStream = btSocket?.outputStream
+        } catch (e: IOException) {
+            //Error
+        }
+
+    }
+
+    open fun write(bytes: Int?) {
+        try {
+            outputStream?.write(bytes!!)
+        } catch (e: IOException) {
+            //Error
+        }
+    }
 }
