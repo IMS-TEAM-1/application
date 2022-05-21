14865281be811b91b36aa3334c4ede67d9794080
Falco1997 <filipgolf1997@gmail.com>
2022-05-21 20:27:01 +0200

    No longer crashes in ControllerFragment

--- app/src/main/java/com/example/ims3000/ui/fragments/ControllerFragment.kt
+++ app/src/main/java/com/example/ims3000/ui/fragments/ControllerFragment.kt
@@ -7,11 +7,13 @@ import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.MotionEvent
+import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.ims3000.R
 import com.example.ims3000.data.remote.MowerStatus
 import com.example.ims3000.databinding.FragmentControllerBinding
@@ -54,8 +56,9 @@ class ControllerFragment : Fragment(R.layout.fragment_controller),EasyPermission
     }
 
     @SuppressLint("ClickableViewAccessibility")
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view ,savedInstanceState)
+        viewModel = ViewModelProvider(requireActivity())[ControllerViewModel::class.java]
 
         if (ActivityCompat.checkSelfPermission(requireContext(),
                 Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED
